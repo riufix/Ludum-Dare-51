648157efd123324ac81796c0b2f892cf6b20d2c8
enguerrand <enguerrand.cousin@gmail.com>
2022-10-01 16:58:42 +0200

    chevalress attack

---
+++ Ludum/Assets/Scenes/Enguerrand/enemyLife.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyLife : MonoBehaviour
+{
+    public Animator anim;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        anim.SetTrigger("");//put the name of this animation here
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died");
+        //die anim
+        anim.SetBool("", true);
+        //desactive ennemy
+        this.enabled = false;
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
