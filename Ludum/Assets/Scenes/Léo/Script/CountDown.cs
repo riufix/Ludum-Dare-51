2c065974923566dddaae8fe44002ef3b8166f1a7
LeoLelann <112940420+LeoLelann@users.noreply.github.com>
2022-10-03 08:31:05 +0200

    Character Swap movement

--- Ludum/Assets/Scenes/Léo/Script/CountDown.cs
+++ Ludum/Assets/Scenes/Léo/Script/CountDown.cs
@@ -4,24 +4,67 @@ using UnityEngine;
 
 public class CountDown : MonoBehaviour
 {
-    float currentTime = 0f;
-    float startingTime = 10f;
 
+    [SerializeField] private float currentTime = 0f;
+    private float startingTime = 10f;
+
+    public float passifRange = 1.5f;
+    
+    public Transform passifpoint;
+    public GameObject Player;
+    public GameObject Assassin;
+    public GameObject Bagarreur;
+    public GameObject Chevalresse;
     void Start()
     {
         currentTime = startingTime;
-    }
+        GameObject gameObject = Instantiate(Assassin);
+        GameObject gameObject1 = Instantiate(Bagarreur);
+        GameObject gameObject2 = Instantiate(Chevalresse);
 
-    // Update is called once per frame
+        gameObject.GetComponent<Character>().enabled = false;
+        gameObject1.GetComponent<Character>().enabled = false;
+        gameObject2.GetComponent<Character>().enabled = false;
+
+        this.Assassin.SetActive(true);
+        this.Bagarreur.SetActive(true);
+        this.Chevalresse.SetActive(true);
+
+    }
     void Update()
     {
         currentTime -= 1 * Time.deltaTime;
-        print (currentTime);
 
         if(currentTime <= 0)
         {
-            //change player
+            int num = Random.Range(1, 4);
+            if(num == 1)
+            {
+                this.Assassin.GetComponent<Character>().enabled = true;
+                this.Bagarreur.GetComponent<Character>().enabled = false;
+                this.Chevalresse.GetComponent<Character>().enabled = false;
+            }
+            else if(num == 2)
+            {
+                this.Assassin.GetComponent<Character>().enabled = false;
+                this.Bagarreur.GetComponent<Character>().enabled = true;
+                this.Chevalresse.GetComponent<Character>().enabled = false;
+            }
+            else
+            {
+                this.Assassin.GetComponent<Character>().enabled = false;
+                this.Bagarreur.GetComponent<Character>().enabled = false;
+                this.Chevalresse.GetComponent<Character>().enabled = true;
+            }
             currentTime = 10f;
+            print(num);
         }
     }
+
+
+    private void OnDrawGizmosSelected()
+    {
+                Gizmos.DrawWireSphere(passifpoint.position, passifRange);
+    }
+
 }
